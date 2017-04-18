3d3770590ccfa523412ccd68b1110a717e383d1c
SimonGustafsson <simon@simongustafsson.com>
2017-04-18 12:23:53 +0200

    fix javascript only mobile

--- _site/js/main.js
+++ _site/js/main.js
@@ -2,3 +2,10 @@
 $('.menu-item').on('click', function(){
       $('.menu--primary').toggleClass('menu-open');
 });
+
+ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+    }
